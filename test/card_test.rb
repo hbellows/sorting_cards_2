a9aff9987fe7313714baeb0d93d8368f0f9ac210
Harper Bellows <harper.bellows@gmail.com>
2018-07-02 17:44:10 -0600

    add new sort test

--- test/card_test.rb
+++ test/card_test.rb
@@ -9,11 +9,17 @@ class CardTest < Minitest::Test
 
     assert_instance_of Card, card
   end
-  
+
   def test_it_has_attributes
     card = Card.new('Ace', 'Spades')
 
     assert_equal 'Ace', card.value
     assert_equal 'Spades', card.suit
+  end
+
+  def test_it_has_a_total_value
+    card = Card.new("Ace", "Spades")
+
+    assert_equal 15, card.total_value
   end
 end
