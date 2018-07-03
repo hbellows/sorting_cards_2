96039b53d7b2e9c1f40cb770a230425460fe3954
Harper Bellows <harper.bellows@gmail.com>
2018-07-03 07:51:34 -0600

    finalize sort method

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,40 +12,15 @@ class Deck
   end
 
   def sort
-    card_value = @cards.each do |card|
-      require "pry"; binding.pry
-      @deck_map[card.value].to_f + @deck_map[card.suit]
-      end
-        @cards.map.with_index do |card, index|
-          if card_value[index + 0] > card_value[index + 1]
-            @cards[index], @cards[index +1] = @cards[index + 1], @cards[index]
-          # else
-          #   @cards[index + 0] = @cards[index + 1]
-      # require "pry"; binding.pry
-        # if card_value > index_value
+    (@cards.length - 1).times do |index|
+      if @cards[index].total_value > @cards[index +1].total_value
+        @cards[index], @cards[index + 1] = @cards[index + 1], @cards[index]
         end
-      end
+    end
+    @cards
   end
 
   def merge_sort
   end
 
-  # def find_card_values
-  #   total_card_value = @cards.map do |card|
-  #     @deck_map[card.value] + @deck_map[card.suit]
-  #   end
-  # end
-
 end
-
-# card_1 = Card.new("4","Hearts")
-# card_2 = Card.new("Jack", "Clubs")
-# card_3 = Card.new("5", "Diamonds")
-# card_4 = Card.new("Ace", "Spades")
-# card_5 = Card.new("Ace", "Diamonds")
-# deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
-#
-# # p deck.find_card_suits
-# # p deck.find_card_values
-#
-# p deck.find_card_values
